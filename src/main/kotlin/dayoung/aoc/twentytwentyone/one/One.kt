6f83cd865b4d994a3dfeb5eaa209e5169c814782
David Young <ohnoitsyou7043@gmail.com>
2021-12-01 00:19:40 -0500

    Add initial solutions to day 1

--- src/main/kotlin/dayoung/aoc/twentytwentyone/one/One.kt
+++ src/main/kotlin/dayoung/aoc/twentytwentyone/one/One.kt
@@ -1,14 +1,30 @@
 package dayoung.aoc.twentytwentyone.one
 
 class One {
+    private val input = this::class.java.getResourceAsStream("/one.txt").bufferedReader().readLines().map(String::toInt)
     private fun part1(): Int {
-
-        return 1
+        var previous = input[0]
+        var count = 0
+        for(value in input) {
+            if(value > previous) {
+                count++
+            }
+            previous = value
+        }
+        return count
     }
 
     private fun part2(): Int {
+        var previous = input[0] + input[1] + input[2]
+        var count = 0
+        for(chunk in input.windowed(3)) {
+            if(chunk.sum() > previous) {
+                count++
+            }
+            previous = chunk.sum()
+        }
 
-        return 1
+        return count
     }
 
     fun solve(): Pair<Int, Int> =
