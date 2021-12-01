83ddc8f276830eacb86fcab34af5b91677c8d805
David Young <ohnoitsyou7043@gmail.com>
2021-12-01 00:27:46 -0500

    Change solution for day 1
    Optimized solutions to be one-liners

--- src/main/kotlin/dayoung/aoc/twentytwentyone/one/One.kt
+++ src/main/kotlin/dayoung/aoc/twentytwentyone/one/One.kt
@@ -1,32 +1,13 @@
 package dayoung.aoc.twentytwentyone.one
 
 class One {
-    private val input = this::class.java.getResourceAsStream("/one.txt").bufferedReader().readLines().map(String::toInt)
-    private fun part1(): Int {
-        var previous = input[0]
-        var count = 0
-        for(value in input) {
-            if(value > previous) {
-                count++
-            }
-            previous = value
-        }
-        return count
-    }
+  private val input = this::class.java.getResourceAsStream("/one.txt")!!.bufferedReader().readLines().map(String::toInt)
+  private fun part1(): Int =
+    input.windowed(2).count { (first: Int, second: Int) -> second > first }
 
-    private fun part2(): Int {
-        var previous = input[0] + input[1] + input[2]
-        var count = 0
-        for(chunk in input.windowed(3)) {
-            if(chunk.sum() > previous) {
-                count++
-            }
-            previous = chunk.sum()
-        }
+  private fun part2(): Int =
+    input.windowed(3).map { it.sum() }.windowed(2).count { (first: Int, second: Int) -> second > first }
 
-        return count
-    }
-
-    fun solve(): Pair<Int, Int> =
-        Pair(part1(), part2())
+  fun solve(): Pair<Int, Int> =
+    Pair(part1(), part2())
 }
